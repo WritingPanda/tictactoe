c3bb94e083abbf3b6632b739c19c5a1948051a66
Omar Quimbaya <omar.quimbaya4@gmail.com>
2014-10-16 17:37:00 -0500

    First commit

---
+++ tictactoe/tictactoe.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int age = 28;
+	printf("     |     |     \n");
+	printf("     |     |     \n");
+	printf("_____|_____|_____\n");
+	printf("     |     |     \n");
+	printf("     |     |     \n");
+	printf("_____|_____|_____\n");
+	printf("     |     |     \n");
+	printf("     |     |     \n");
+	printf("     |     |     \n");
+	getch();
+	return 0;
+}
