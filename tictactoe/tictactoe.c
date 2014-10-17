b9bc9221ca695cdda8f6ab226e1bfa744ecc3289
Omar Quimbaya <omar.quimbaya4@gmail.com>
2014-10-17 15:50:57 -0500

    Using different modifiers for variables saved to memory.

--- tictactoe/tictactoe.c
+++ tictactoe/tictactoe.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+void tempfunc() {
+	double var1 = 1.1;
+	float var2 = 2.2;
+	short int var3 = 3;
+	long var4 = 4;
+
+	printf("Variable: %04f\n Size: %u\n", var1, sizeof var1);
+	printf("Variable: %04f\n Size: %u\n", var2, sizeof var2);
+	printf("Variable: %04hi\n Size: %u\n", var3, sizeof var3);
+	printf("Variable: %04li\n Size: %u\n", var4, sizeof var4);
+}
+
 int main(int argc, char *argv[]) {
+
+	tempfunc();
+	getch();
+	/*
 	int age = 28;
 	printf("     |     |     \n");
 	printf("     |     |     \n");
@@ -13,4 +29,5 @@ int main(int argc, char *argv[]) {
 	printf("     |     |     \n");
 	getch();
 	return 0;
+	*/
 }
